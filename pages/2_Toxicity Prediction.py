9a7a7394f83a047dd7cd1c8836cc954081178124
NVerner <99597964+Varnhover@users.noreply.github.com>
2023-11-21 20:22:44 +0300

    Update 2_Toxicity Prediction.py

--- pages/2_Toxicity Prediction.py
+++ pages/2_Toxicity Prediction.py
@@ -20,6 +20,6 @@ st.button("Предсказать токсичность")
 if st.button:
     #os.system('streamlit run https://github.com/Varnhover/D4Tool/blob/main/ToxPred/etoxpred_predict.py')
     #os.system('git clone https://github.com/Varnhover/D4Tool')
-    os.system('pip install pandas, joblib')
+    os.system('pip install joblib')
     os.system('python D4Tool/ToxPred/etoxpred_predict.py --datafile test.smi --modelfile dbs/etoxpred_best_model.joblib --outputfile results.csv')
     #st.write(pd.read_csv('ToxPred/results(Rasagiline).csv'))
