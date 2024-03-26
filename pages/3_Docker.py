9b089a11785a57159f2ebe035ff1dde73186d2db
NVerner <99597964+Varnhover@users.noreply.github.com>
2024-03-26 17:14:46 +0300

    Update 3_Docker.py

--- pages/3_Docker.py
+++ pages/3_Docker.py
@@ -24,7 +24,8 @@ if dwnld:
   download_results = True
 else:
   download_results = False
-st.write("from remote url")
-files = ["https://files.rcsb.org/download/3PTB.pdb", "https://files.rcsb.org/download/1LOL.pdb"]
+
 st_molstar_auto(files, key="6", height="320px")
 if st.button("Start docking"):
+  st.write("from remote url")
+  files = ["https://files.rcsb.org/download/3PTB.pdb", "https://files.rcsb.org/download/1LOL.pdb"]
