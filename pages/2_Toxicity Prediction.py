65c230a62ae19aaee57d1d2dc0e1b59888eb663d
NVerner <99597964+Varnhover@users.noreply.github.com>
2024-03-26 16:16:05 +0300

    Update 2_Toxicity Prediction.py

--- pages/2_Toxicity Prediction.py
+++ pages/2_Toxicity Prediction.py
@@ -10,11 +10,6 @@ st.set_page_config(page_title="D4Tool",page_icon="💊")
 """
 
 smi = st.file_uploader("Your molecules .smi file")
-def save_uploaded_file(uploadedfile):
-  with open(os.path.join("dbs",uploadedfile.name),"wb") as f:
-     f.write(uploadedfile.getbuffer())
-  return st.success("Saved file :{} in dbs".format(uploadedfile.name))
-save_uploaded_file(smi)
 
 #f = open('results.smi', 'w')
 #f.write("1")
@@ -22,8 +17,7 @@ save_uploaded_file(smi)
 #-- browser.gatherUsageStats false
 
 if st.button("Start prediction"):
-    if smi is None:
-        smi = "test.smi"
+    smi = "test.smi"
     os.system(f'python ToxPred/etoxpred_predict.py --datafile dbs/.name --modelfile dbs/etoxpred_best_model.joblib --outputfile results.csv')
     st.write(pandas.read_csv('results.csv'))
 
