f394aa3922516f36e5d40d6e038283fbcf4fecd3
NVerner <99597964+Varnhover@users.noreply.github.com>
2024-03-26 17:01:54 +0300

    Update 3_Docker.py

--- pages/3_Docker.py
+++ pages/3_Docker.py
@@ -6,8 +6,7 @@ import sklearn #==0.23.2
 import requests
 import time
 from random import random
-os.system('pip install streamlit-molstar')
-
+from streamlit_molstar.docking import st_molstar_docking
 
 
 st.set_page_config(page_title="D4Tool",page_icon="💊")
@@ -27,5 +26,4 @@ if dwnld:
 else:
   download_results = False
 if st.button("Start docking"):
-  from streamlit_molstar.docking import st_molstar_docking
   st_molstar_dockng('dbs/2zy1_protein.pdb', 'dbs/docking.2zy1.0.sdf',  gt_ligand_file_path='dbs/2zy1_ligand.sdf', key="5", height=240)
